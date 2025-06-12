93684647c13ae6ccb03583cd416b20534195e1e9
shlok italiya <152770633+shlokitaliya@users.noreply.github.com>
2025-06-12 15:13:47 +0530

    Delete amazon-clone directory

--- amazon-clone/scripts/checkout/checkoutHeaderHtml.js
+++
@@ -1,9 +0,0 @@
-import { cart } from "../../data/cart.js";
-export function updateCartQuantityCheckoutPageHeader(){
-    let totalQuantity = 0;
-  
-    cart.forEach((items)=>{
-        totalQuantity += items.quantity;
-    });
-    document.querySelector('.cart-quantity-checkout-page-header').innerHTML=totalQuantity + ` Items`;
-  }
